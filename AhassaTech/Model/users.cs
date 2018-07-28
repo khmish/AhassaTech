ac13695f468ac5c0e6ef592624d80709707f0057
Mohammed Sadiq <m.abather@gmail.com>
2018-07-28 14:32:01 +0300

    update useres class
    add the required Methods for the Model.

--- AhassaTech/Model/users.cs
+++ AhassaTech/Model/users.cs
@@ -3,8 +3,12 @@ namespace AhassaTech.Model
 {
     public class users
     {
-        public users()
-        {
+        public int userId { set; get; }
+        public string userFullName { set; get; }
+        public string userNickName { set; get; }
+        public int user_type { set; get; }
+        public int birthDate { set; get; }
+        public bool gender { set; get; }
+        public string phoneNumber {set; get;}
         }
-    }
 }
