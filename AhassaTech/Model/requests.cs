4e978785b600f8f2b1980c4c67069c9d84997942
AQeeL <aqeel123Gi@users.noreply.github.com>
2018-07-29 16:01:56 +0300

    Adding requests model.

---
+++ AhassaTech/Model/requests.cs
@@ -0,0 +1,12 @@
+﻿namespace AhassaTech.Model
+{
+    public class requests
+    {
+        public int Id { set; get; }
+        public string name { set; get; }
+        public string email { set; get; }
+        public int user_id { set; get; }
+        public string approved { set; get; }
+        public string comments { set; get; }
+    }
+}
